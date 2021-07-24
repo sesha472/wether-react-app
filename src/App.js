9598426f23ecb62ba69234ca2424f5185dc642e2
sesha <sesha472@gmail.com>
2021-07-24 19:29:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react'
+import Wether from './components/Wether';
 
 function App() {
+  const [data, setdata] = useState("");
+   const outputtemparature=(tempdata)=>{
+    setdata(tempdata);
+   }
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Wether tepmhandler={outputtemparature} />
+       <h1>temparature is {data} </h1>
     </div>
   );
 }
---
+++ src/components/Wether.js
@@ -0,0 +1,37 @@
+import React,{useState} from 'react'
+
+const Wether = (props) => {
+    const [data, setdata] = useState("");
+    const [temp, settemp] = useState("");
+  
+    const submithandler=(e)=>{
+         e.preventDefault();
+         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=d885aa1d783fd13a55050afeef620fcb`)
+          .then(res=>res.json())
+          .then(result=> {
+            const kelvin=result.main.temp;
+            const celsius=kelvin-273.15
+            settemp( Math.round(celsius) +"C");
+             })
+          props.tepmhandler(temp);
+    }
+
+    return (
+        <div>
+            <form onSubmit={submithandler}>
+                <input type="text" value={data} onChange={(e)=>setdata(e.target.value)} />
+            <button>submit</button>
+            </form>
+            
+        </div>
+    )
+}
+
+export default Wether
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M master
+// git remote add origin https://github.com/sesha472/wether-react-app.git
+// git push -u origin master
