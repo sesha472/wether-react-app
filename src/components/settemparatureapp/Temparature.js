d6a6a8346043bedf7a2b28fc60058bff5f2c3241
sesha <sesha472@gmail.com>
2021-08-29 16:33:19 +0530

    temparature chnaging functinolity added

--- src/components/settemparatureapp/Temparature.js
+++ src/components/settemparatureapp/Temparature.js
@@ -6,18 +6,15 @@ const Temparature = () => {
       const tempdisplay="cold";
       const hotdisplay="hot"
     return (
-        <body>
      <div className="container">
-        <div className="temparaturedisplaycontainer">
-            <div className=  {`temp  ${ (temparature>15) ? hotdisplay:tempdisplay} `}> {temparature}*C </div>
-        </div>
+           
+         <div className=  {`temp  ${ (temparature>15) ? hotdisplay:tempdisplay} `}> {temparature}*C </div>
 
         <div className="buttonwraper">
             <button className="button1" onClick={()=>settemparature(temparature+1)} >+</button>
             <button  className="button2" onClick={()=>settemparature(temparature-1)} >-</button>
         </div>
      </div>
-     </body>
     )
 }
 
